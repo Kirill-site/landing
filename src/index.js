2a9f5a0813280f4d53d63bf263cda6426b8a5093
Kirill-it1 <90218602+Kirill-it1@users.noreply.github.com>
2023-11-12 19:57:14 +0300

    upd

--- src/index.js
+++ src/index.js
@@ -6,3 +6,23 @@ import MainSwiperPortfolio from './components/MainSwiperPortfolio';
 const main_swiper_portfolio = ReactDOMClient.createRoot(document.querySelector('.portfolio-slider-block'));
 
 main_swiper_portfolio.render(<MainSwiperPortfolio/>);
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+	anchor.addEventListener('click', function (e) {
+			e.preventDefault();
+			const href = this.getAttribute('href');
+			let el = '';
+			if (href !== '#top') {
+				el = document.querySelector(this.getAttribute('href'));
+				el.scrollIntoView({
+					behavior: 'smooth'
+				});
+			} else {
+				el = document.querySelector('.header');
+				el.scrollIntoView({
+					behavior: 'smooth'
+				});
+			}
+
+	});
+});
